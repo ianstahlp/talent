b2800d2cbd7f502da3ad3e574f4ed2f5610626f0
Omar Patel <ianpstahl@gmail.com>
2019-11-24 12:48:26 -0800

    Stuff

--- server/models/actor-model.js
+++ server/models/actor-model.js
@@ -1,11 +1,11 @@
-const mongoos = require('mongoose'); 
+const mongoose = require('mongoose'); 
 const Schema = mongoose.Schema;
 
 const Actor =  new Schema(
     {
         first_name: {type: String, required: true},
         last_name: {type: String, required: true},
-        middle_name: {type: String, required: false, default: NULL},
+        middle_name: {type: String, required: false, default: null},
         headshot: {type: String, required: false},
     },
     {timestamps: true},
