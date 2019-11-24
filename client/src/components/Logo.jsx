dd862c783ce354f80718f5a34027afa9bac9e3e5
Omar Patel <ianpstahl@gmail.com>
2019-11-24 13:08:02 -0800

    More pushes to meet unmet dependencies

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+
+import logo from '../logo.svg';
+
+const Wrapper = styled.a.attrs({
+    className: 'navbar-brand',
+})``;
+
+class Logo extends Component {
+    render() {
+        return (
+            <Wrapper href="https://google.com">
+                <img src={logo} width="50" height="50" art="google.com" />
+            </Wrapper>
+        )
+    }
+}
+
+
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import logo from '../logo.svg'
+
+const Wrapper = styled.a.attrs({
+    className: 'navbar-brand',
+})``
+
+class Logo extends Component {
+    render() {
+        return (
+            <Wrapper href="https://sambarros.com">
+                <img src={logo} width="50" height="50" alt="sambarros.com" />
+            </Wrapper>
+        )
+    }
+}
+
+export default Logo
